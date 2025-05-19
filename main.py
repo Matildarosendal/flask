7fc6746394d0adf7258b75af72fa2fd20ff8f365
Mikael Helin <mikael@tuf17.mikaelhelin.com>
2025-05-19 16:49:11 +0200

    update

--- main.py
+++ main.py
@@ -14,28 +14,28 @@ def health():
 
 @app.route("/nums")
 def num_numbers():
-    numbers_param = request.args.get('numbers')
+    numbers_param = request.args.get("numbers")
     if not numbers_param:
         return jsonify(error="Missing 'numbers' parameter"), 400
-    numbers = numbers_param.split(',')
+    numbers = numbers_param.split(",")
     return jsonify(num=len(numbers)), 200
 
 @app.route("/sum")
 def sum_numbers():
-    numbers_param = request.args.get('numbers')
+    numbers_param = request.args.get("numbers")
     mysum = 0
     if numbers_param:
-        numbers = numbers_param.split(',')
+        numbers = numbers_param.split(",")
         for num in numbers:
             mysum += int(num)
     return jsonify(sum=mysum), 200
 
 @app.route("/max")
 def max_numbers():
-    numbers_param = request.args.get('numbers')
+    numbers_param = request.args.get("numbers")
     if not numbers_param:
         return jsonify(error="Missing 'numbers' parameter"), 400
-    numbers = numbers_param.split(',')
+    numbers = numbers_param.split(",")
     max_num = max(numbers)
     return jsonify(max=max_num), 200
 
