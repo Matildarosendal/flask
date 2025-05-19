eaa5de7b51d2dec88bf23ddf1200bea685f26d0b
Mikael Helin <mikael@tuf17.mikaelhelin.com>
2025-05-19 16:24:39 +0200

    example

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
 def health():
     return jsonify(status="ok"), 200
 
-@app.route("/numbers")
+@app.route("/nums")
 def num_numbers():
     numbers_param = request.args.get('numbers')
     if not numbers_param:
@@ -20,6 +20,24 @@ def num_numbers():
     numbers = numbers_param.split(',')
     return jsonify(num=len(numbers)), 200
 
+@app.route("/sum")
+def sum_numbers():
+    numbers_param = request.args.get('numbers')
+    mysum = 0
+    if numbers_param:
+        numbers = numbers_param.split(',')
+        for num in numbers:
+            mysum += int(num)
+    return jsonify(sum=mysum), 200
+
+@app.route("/max")
+def max_numbers():
+    numbers_param = request.args.get('numbers')
+    if not numbers_param:
+        return jsonify(error="Missing 'numbers' parameter"), 400
+    numbers = numbers_param.split(',')
+    max_num = max(numbers)
+    return jsonify(max=max_num), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=True)
