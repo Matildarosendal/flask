c33be50bcc663e3eca470f9054e5478cce318b81
Mikael Helin <mikael@tuf17.mikaelhelin.com>
2025-05-19 16:18:27 +0200

    example

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,9 +8,18 @@ def index():
         message="Flask API by Matilda"
     ), 200
 
-@app.route('/health', methods=['GET'])
+@app.route("/health", methods=["GET"])
 def health():
     return jsonify(status="ok"), 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route("/numbers")
+def num_numbers():
+    numbers_param = request.args.get('numbers')
+    if not numbers_param:
+        return jsonify(error="Missing 'numbers' parameter"), 400
+    numbers = numbers_param.split(',')
+    return jsonify(num=len(numbers)), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=True)
